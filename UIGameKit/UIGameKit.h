d40da136ca3c1aec1d40c601cae760a5bde1c276
Pedro Farina <pedrogfarina@gmail.com>
2019-06-26 23:27:00 -0300

    Initial Commit

---
+++ UIGameKit/UIGameKit.h
@@ -0,0 +1,19 @@
+//
+//  UIGameKit.h
+//  UIGameKit
+//
+//  Created by Pedro Giuliano Farina on 26/06/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIGameKit.
+FOUNDATION_EXPORT double UIGameKitVersionNumber;
+
+//! Project version string for UIGameKit.
+FOUNDATION_EXPORT const unsigned char UIGameKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIGameKit/PublicHeader.h>
+
+
